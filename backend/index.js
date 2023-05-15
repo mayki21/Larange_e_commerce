09273d82910946bb78ebae27cc2848c85c2b6637
mayank <mayank.khobragade9891@gmail.com>
2023-05-15 23:28:20 +0530

    day-3

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,36 @@
 const express=require("express")
 require("dotenv").config()
 const connection=require("./connection/db");
+const cartRoute=require("./routes/cartroute")
+const jeansRouter=require("./routes/jeansroute")
+const ShoesRoute=require("./routes/shoesroute")
+const topsRoute=require("./routes/toproute")
+const userRouter=require("./routes/userroute")
+const auth=require("./middleware/auth")
 const cors=require("cors")
 const app=express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use("/",(req,res)=>{
-    res.status(200).send("heloo")
-})
+// app.use("/",(req,res)=>{
+//     res.status(200).send("heloo")
+// })
+
+
+
+
+
+app.use("/users", userRouter)
+
+app.use("/jeans", jeansRouter)
+
+app.use("/tops", topsRoute)
+
+app.use("/shoes", ShoesRoute)
+// app.use(auth);
+app.use("/cart",cartRoute)
+
 
 
 app.listen(process.env.port,async()=>{
